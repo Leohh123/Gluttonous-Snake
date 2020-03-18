34dfb5750fd07de8e723ae6cc247235655a0b171
Leohh <948750310@qq.com>
2020-03-19 01:54:33 +0800

    add pixel arts

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const N = 80;
 const M = 60;
 const DX = [-1, 0, 0, 1];
 const DY = [0, -1, 1, 0];
+const DS2 = ["l", "u", "d", "r"];
+const DS3 = ["h", "v", "lu", "ru", "ld", "rd"];
 const DRS = (function() {
     var t = [];
     t[97] = 0;
@@ -28,10 +30,30 @@ var Utils = (function() {
         };
     }
     Utils.prototype.randint = function(x, y) {
-        return x + Math.floor(Math.random() * (y - x));
+        return x + Math.floor(Math.random() * (y - x + 1));
     };
-    Utils.prototype.eq = function(a, b) {
-        return a[0] == b[0] && a[1] == b[1];
+    Utils.prototype.eq = function(...a) {
+        for (var i = 1; i < a.length; i++) {
+            if (a[0][0] != a[i][0] || a[0][1] != a[i][1]) {
+                return false;
+            }
+        }
+        return true;
+    };
+    Utils.prototype.mov = function(a, d) {
+        if (d == "u") {
+            return [a[0], a[1] - 1];
+        }
+        if (d == "d") {
+            return [a[0], a[1] + 1];
+        }
+        if (d == "l") {
+            return [a[0] - 1, a[1]];
+        }
+        if (d == "r") {
+            return [a[0] + 1, a[1]];
+        }
+        console.error("Utils.mov", "Invalid direction");
     };
     Utils.prototype.unique = function(a) {
         var ans = [];
@@ -41,6 +63,54 @@ var Utils = (function() {
             }
         }
         return ans;
+    };
+    Utils.prototype.checkShape = function(a, b, c) {
+        if (!a || !c) {
+            var t = !a ? c : a;
+            for (var d of DS2) {
+                if (this.eq(this.mov(t, d), b)) {
+                    return d;
+                }
+            }
+            console.error("Utils.checkShape", "Head or tail: no answer");
+        }
+        if (
+            this.eq(this.mov(a, "r"), this.mov(c, "l"), b) ||
+            this.eq(this.mov(c, "r"), this.mov(a, "l"), b)
+        ) {
+            return "h";
+        }
+        if (
+            this.eq(this.mov(a, "d"), this.mov(c, "u"), b) ||
+            this.eq(this.mov(c, "d"), this.mov(a, "u"), b)
+        ) {
+            return "v";
+        }
+        if (
+            this.eq(this.mov(a, "l"), this.mov(c, "d"), b) ||
+            this.eq(this.mov(c, "l"), this.mov(a, "d"), b)
+        ) {
+            return "lu";
+        }
+        if (
+            this.eq(this.mov(a, "r"), this.mov(c, "d"), b) ||
+            this.eq(this.mov(c, "r"), this.mov(a, "d"), b)
+        ) {
+            return "ru";
+        }
+        if (
+            this.eq(this.mov(a, "l"), this.mov(c, "u"), b) ||
+            this.eq(this.mov(c, "l"), this.mov(a, "u"), b)
+        ) {
+            return "ld";
+        }
+        if (
+            this.eq(this.mov(a, "r"), this.mov(c, "u"), b) ||
+            this.eq(this.mov(c, "r"), this.mov(a, "u"), b)
+        ) {
+            return "rd";
+        }
+        console.error("Utils.checkShape", "Body: no answer");
     };
     return new Utils();
 })();
@@ -128,53 +198,111 @@ var Game = (function() {
     return Game;
 })();
 
+var Resource = (function() {
+    function Resource() {
+        this.res = {};
+    }
+    Resource.prototype.add = function(name, src, type = "image") {
+        if (!this.res[name]) {
+            var obj;
+            if (type == "image") {
+                obj = new Image();
+                obj.src = src;
+            }
+            this.res[name] = obj;
+        }
+    };
+    Resource.prototype.get = function(name) {
+        return this.res[name];
+    };
+    return Resource;
+})();
+
 var Renderer = (function() {
     function Renderer(_run) {
         this._run = _run;
-        this.scale = 20;
+        this.scale = 16;
         this.canvas = document.getElementById("canvas");
         canvas.width = N * this.scale;
         canvas.height = M * this.scale;
         this.ctx = canvas.getContext("2d");
+        this.resourse = new Resource();
         CanvasRenderingContext2D.prototype._fill = function(color) {
             this.save();
             this.beginPath();
             this.rect(0, 0, canvas.width, canvas.height);
             this.closePath();
             this.fillStyle = color;
-            this.strokeStyle = "";
             this.fill();
             this.restore();
         };
-        CanvasRenderingContext2D.prototype._rect = function(color, pos) {
+        CanvasRenderingContext2D.prototype._rect = function(color, x, y, w, h) {
             this.save();
             this.beginPath();
-            this.rect(...pos);
+            this.rect(x, y, w, h);
             this.closePath();
             this.fillStyle = color;
-            this.strokeStyle = "";
             this.fill();
+            this.restore();
+        };
+        CanvasRenderingContext2D.prototype._image = function(img, x, y, w, h) {
+            this.save();
+            this.imageSmoothingEnabled = false;
+            this.drawImage(img, x, y, w, h);
             this.restore();
         };
     }
+    Renderer.prototype.init = function() {
+        this.resourse.add("grass", "./image/grass.png");
+        this.resourse.add("food", "./image/food.png");
+        for (var d of DS2) {
+            this.resourse.add(`head${d}`, `./image/head${d}.png`);
+            this.resourse.add(`tail${d}`, `./image/tail${d}.png`);
+        }
+        for (var d of DS3) {
+            this.resourse.add(`body${d}`, `./image/body${d}.png`);
+        }
+    };
     Renderer.prototype.render = function(positions, food) {
-        this.ctx._fill("rgb(0, 0, 0)");
-        for (var a of positions) {
+        this.ctx._fill("rgb(255,255,255)");
+        for (var i = 0; i < N; i += 8) {
+            for (var j = 0; j < M; j += 8) {
+                this.ctx._image(
+                    this.resourse.get("grass"),
+                    i * this.scale,
+                    j * this.scale,
+                    8 * this.scale,
+                    8 * this.scale
+                );
+            }
+        }
+        for (var i = 0; i < positions.length; i++) {
+            var sp = Utils.checkShape(
+                positions[i - 1],
+                positions[i],
+                positions[i + 1]
+            );
             var pos = [
-                a[0] * this.scale,
-                a[1] * this.scale,
+                positions[i][0] * this.scale,
+                positions[i][1] * this.scale,
                 this.scale,
                 this.scale
             ];
-            this.ctx._rect("rgb(255, 255, 255)", pos);
+            if (i == 0) {
+                this.ctx._image(this.resourse.get(`head${sp}`), ...pos);
+            } else if (i == positions.length - 1) {
+                this.ctx._image(this.resourse.get(`tail${sp}`), ...pos);
+            } else {
+                this.ctx._image(this.resourse.get(`body${sp}`), ...pos);
+            }
         }
-        var fpos = [
+        this.ctx._image(
+            this.resourse.get("food"),
             food[0] * this.scale,
             food[1] * this.scale,
             this.scale,
             this.scale
-        ];
-        this.ctx._rect("rgb(0, 0, 255)", fpos);
+        );
     };
     return Renderer;
 })();
@@ -187,7 +315,6 @@ var Input = (function() {
         document.onkeypress = function(event) {
             var e =
                 event || window.event || arguments.callee.caller.arguments[0];
-            console.log("press", e.keyCode);
             if (e && _this.pressEvent[e.keyCode]) {
                 _this.pressEvent[e.keyCode](e.keyCode);
             }
@@ -209,6 +336,7 @@ var Run = (function() {
         this.input = new Input(this);
     }
     Run.prototype.init = function() {
+        this.renderer.init();
         var _this = this;
         this.input.setEvent(
             function() {
